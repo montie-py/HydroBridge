5d8c5051a2fa815ffdd910fca4b2ac7dfc082fff
michteau <montiepy@gmail.com>
2026-02-15 22:48:36 -0700

    transforming client.py into a constant polling client with a 5 sec interval

--- simulation/client.py
+++ simulation/client.py
@@ -1,5 +1,18 @@
 from pymodbus.client import ModbusTcpClient
+import time
 
 client = ModbusTcpClient("127.0.0.1", port=1502)
-client.connect()
-print(client.read_holding_registers(0, count=10))
+
+while True:
+    try:
+        if not client.connected:
+            client.connect()
+        result = client.read_holding_registers(0, count=50)
+        if not result.isError():
+            print(result.registers)
+        else:
+            print("Read error:", result)
+        time.sleep(5)
+
+    except Exception as e:
+        print("Exception: ", e)
