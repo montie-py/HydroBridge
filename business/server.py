92536c70f84c56c909cdc5914ce53213e583f81b
vladimirbodrug <montiepy@gmail.com>
2026-08-30 16:02:18 -0600

    setting up modbus server

--- business/server.py
+++ business/server.py
@@ -1,5 +1,24 @@
 from business.runnable import Runnable
+from easymodbus import modbus_server
+import time
 
 
 class Server(Runnable):
-    pass
+    def run(self):
+        server = modbus_server.ModbusServer()
+        server.listen()
+
+        try:
+            while True:
+                # data stores are plain Python lists, 0-indexed, length 65535
+                server.holdingRegisters.localHoldingRegisters[1] = 4711
+                server.inputRegisters.localInputRegisters[1] = 2500
+                server.coils.localCoils[1] = True
+                server.discreteInputs.localDiscreteInputs[1] = True
+
+                # read what a client wrote
+                print(server.holdingRegisters.localHoldingRegisters[10])
+
+                time.sleep(1)
+        except KeyboardInterrupt:
+            server.close()
--- config/devices_config.py
+++ config/devices_config.py
@@ -11,8 +11,16 @@ class DevicesConfig(BaseModel):
         populate_by_name = True
 
 
+class IntervalConfiguration(BaseModel):
+    polling_loop_pause_mills: int = Field(alias="PollingLoopPauseMills")
+    connection_trying_timeout_mills: int = Field(alias="ConnectionTryingTimeoutMills")
+    polling_loop_interval_mills: int = Field(alias="PollingLoopIntervalMills")
+    retry_delay_mills: int = Field(alias="RetryDelayMills")
+
+
 class AppConfig(BaseModel):
     devices_configuration: list[DevicesConfig] = Field(alias="DevicesConfiguration")
+    interval_configuration: list[DevicesConfig] = Field(alias="IntervalConfiguration")
 
 
 def get_config():
